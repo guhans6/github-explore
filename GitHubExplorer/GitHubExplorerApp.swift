5b9ca60e9ea4e7fbe223568ed2be86a4dfd29702
Guhan <guhans61101@gmail.com>
2026-01-08 16:34:08 +0530

    Initial Commit

---
+++ GitHubExplorer/GitHubExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExplorerApp.swift
+//  GitHubExplorer
+//
+//  Created by Guhan on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
